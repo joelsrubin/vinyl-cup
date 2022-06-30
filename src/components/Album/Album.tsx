4a87a004045b4226711a4e94c56c837968d0eb49
Joel Rubin <joelsrubin@joelrubxqw3vh0k.lan>
2022-06-30 16:01:55 -0500

    update remove mapping

--- src/components/Album/Album.tsx
+++ src/components/Album/Album.tsx
@@ -9,17 +9,16 @@ export function Album({
   isSelected: boolean;
   clickHandler: (album: Release) => void;
 }) {
+  console.log(data.basic_information.thumb);
   return (
     <div
       className={isSelected ? "selected" : "card"}
       onClick={() => clickHandler(data)}
     >
-      {data.basic_information.artists.map((_, i) => (
-        <div key={i}>
-          <h4>{data.basic_information.title}</h4>
-          <img src={data.basic_information.thumb} height={100} width={100} />
-        </div>
-      ))}
+      <div>
+        <h4>{data.basic_information.title}</h4>
+        <img src={data.basic_information.thumb} height={100} width={100} />
+      </div>
     </div>
   );
 }
