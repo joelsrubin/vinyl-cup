62484a8b33797d79f8ff7edc5f7400d19774f30f
Joel Rubin <joelsrubin@joelrubxqw3vh0k.lan>
2022-06-30 14:22:35 -0500

    add hover state

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,59 @@ import { Album } from "./components/Album/Album";
 import { getCollections } from "../api";
 function App() {
   const [data, setData] = React.useState<Release[]>([]);
-
+  const [searchedAlbums, setSearchedAlbums] = React.useState<Release[]>([]);
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [selected, setSelected] = React.useState<Release | null>(null);
   async function fetchData() {
     const json = await getCollections();
     setData(json.releases);
   }
 
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setSearchTerm(e.target.value);
+    const filteredAlbums = data.filter(
+      (album) =>
+        album.basic_information.title.toLowerCase().includes(searchTerm) ||
+        album.basic_information.artists
+          .map((artist) => artist.name.toLowerCase())
+          .join(" ")
+          .includes(searchTerm)
+    );
+    setSearchedAlbums(filteredAlbums);
+  }
+
+  function handleClick(album: Release) {
+    setSelected(album);
+  }
+
   React.useEffect(() => {
     !data.length && fetchData();
-  }, []);
+  }, [data]);
+
+  React.useEffect(() => {
+    if (searchTerm.length === 0) {
+      setSearchedAlbums([]);
+    }
+  }, [searchTerm]);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Vinyl Library</h1>
+        <input
+          type="text"
+          placeholder="search by artist or title"
+          onChange={handleChange}
+        />
       </header>
       <div className="albums">
-        {data.map((release, i) => (
-          <Album data={release} key={i} />
+        {searchedAlbums.map((release, i) => (
+          <Album
+            data={release}
+            isSelected={selected?.id === release.id}
+            key={i}
+            clickHandler={handleClick}
+          />
         ))}
       </div>
     </div>
--- src/components/Album/Album.tsx
+++ src/components/Album/Album.tsx
@@ -1,6 +1,19 @@
-export function Album({ data }: { data: Release }) {
+import * as React from "react";
+
+export function Album({
+  data,
+  clickHandler,
+  isSelected,
+}: {
+  data: Release;
+  isSelected: boolean;
+  clickHandler: (album: Release) => void;
+}) {
   return (
-    <div className="card">
+    <div
+      className={isSelected ? "selected" : "card"}
+      onClick={() => clickHandler(data)}
+    >
       {data.basic_information.artists.map((_, i) => (
         <div key={i}>
           <h4>{data.basic_information.title}</h4>
